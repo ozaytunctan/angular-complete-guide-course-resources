6cf3d92942fe30df1e911a92fb25130465ee6c28
ozaytunctan <ozaytunctan@gmail.com>
2024-12-28 21:01:12 +0300

    component deep dives

--- code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component";
+
+type ServerStatus = 'offline' | 'online' | 'unknown';
 
 @Component({
   selector: 'app-server-status',
@@ -10,7 +12,31 @@ import {DashboardItemComponent} from "../dashboard-item/dashboard-item.component
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: ServerStatus = "online";
+
+  private intervalRef: any;
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.intervalRef = setInterval(() => {
+      const rnd = Math.random();//0-0.99999999
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000)
+  }
+
+  ngOnDestroy() {
+    if (this.intervalRef) {
+      clearInterval(this.intervalRef);
+    }
+  }
+
 
 }
--- code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
+++ code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'form-control',
@@ -13,12 +13,28 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   // encapsulation: ViewEncapsulation.Emulated Shadow dom kapsamındadır.
   host: {
     class: 'form-control',//Host selector e form-control class default olarak ekler.
-
+    '(click)': 'onClickInput()'
   }
 
 })
 export class ControlComponent {
 
   label = input.required<string>();
+
+  // @HostBinding("class") className = "form-control";
+
+  // @HostListener("click")onClickInput(): void {
+  // console.log("form control clicked..");
+  // }
+
+  private el = inject(ElementRef);
+
+  onClickInput() {
+    //programatically host binding and listener
+    // console.log(this.el.nativeElement);
+    // this.el.nativeElement.classList.add('form-control');
+    // this.el.nativeElement.addEventListener('click', this.onClickInput);
+    // this.el.nativeElement.style.color = 'red';
+  }
 
 }
